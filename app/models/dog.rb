9f060522424299d368a9f503d7b76f9a65a7d4fc
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-16 02:01:29 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  
+
   attr_accessor :name, :breed, :age
   @@all=[]
 
   def initialize(name,breed,age)
-    @name = name 
+    @name = name
     @breed = breed
     @age = age 
-  end 
+  end
 end
